6c3353b7420c1e8c84d5204de7e228cfd0b54ee0
jdelaroche94 <jdelaroche94@hotmail.com>
2019-09-29 08:16:11 +1000

    Initial Commit

---
+++ IOTApp-Assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOTApp-Assignment2
+//
+//  Created by Julian A De La Roche on 29/9/19.
+//  Copyright © 2019 Julian A De La Roche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
