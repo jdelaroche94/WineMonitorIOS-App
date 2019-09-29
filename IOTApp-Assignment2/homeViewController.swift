0559a40cb00690a9141c7279ba1ab6f30208ed6e
jdelaroche94 <jdelaroche94@hotmail.com>
2019-09-30 09:30:52 +1000

    We add some changes in the homeViewController. Now the model which analysis temperature and light is working

--- IOTApp-Assignment2/homeViewController.swift
+++ IOTApp-Assignment2/homeViewController.swift
@@ -14,17 +14,23 @@ class homeViewController: UIViewController, DatabaseListener {
     
     @IBOutlet var buttonOutlet: UIButton!
     @IBAction func buttonAction(_ sender: Any) {
-        print(temps)
+        whetherRecommendations()
     }
     
     var temps: [Temperature] = []
     var rgbs: [RGB] = []
     var whetherRecs: [Whether_Recommendation] = []
+    var temperature: Int = -24
+    var red: Int = 0
+    var green: Int = 0
+    var blue: Int = 0
+    let nightLightMaximum: Int = 1000
+    let dayLightMinimum: Int = 1001
+    
     weak var databaseController: DatabaseProtocol?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         databaseController = appDelegate.databaseController
        
@@ -45,6 +51,7 @@ class homeViewController: UIViewController, DatabaseListener {
     var listenerType: ListenerType = ListenerType.all
     func onTemperatureChange(change: DatabaseChange, temperatures: [Temperature]) {
         temps = temperatures
+        
     }
     
     func onRGBChange(change: DatabaseChange, rgbs: [RGB]) {
@@ -54,6 +61,35 @@ class homeViewController: UIViewController, DatabaseListener {
     func onWhetherRecChange(change: DatabaseChange, whetherRecs: [Whether_Recommendation]) {
         self.whetherRecs = whetherRecs
     }
+    
+    func whetherRecommendations(){
+        let numberOfRegs: Int = temps.count
+        if numberOfRegs != 0 {
+            let numberOfRGBs: Int = rgbs.count
+            let temporalTemp: Int = temps[numberOfRegs - 1].tempDegrees
+            if numberOfRGBs != 0 && temporalTemp != temperature{
+                temperature = temporalTemp
+                let tempRed: Int = rgbs[numberOfRGBs - 1].red
+                let tempGreen: Int = rgbs[numberOfRGBs - 1].green
+                let tempBlue: Int = rgbs[numberOfRGBs - 1].blue
+                print("Temp: \(temporalTemp) and Light: \(tempRed)")
+                if tempRed != red || tempGreen != green || tempBlue != blue{
+                    red = tempRed
+                    green = tempGreen
+                    blue = tempBlue
+                    for whetherRec in whetherRecs {
+                        if temperature >= whetherRec.temperature_min && temperature <= whetherRec.temperature_max {
+                            if red >= (whetherRec.light_min - 100) && red <= (whetherRec.light_max + 100) {
+                                print(whetherRec.activity)
+                        }
+                    }
+                    }
+                    
+                }
+            }
+        }
+    }
+    
     
     
     /*
